aff9cd789977b6d58a6871501a8c19f9f3f52558
Fts99 <47905996+Fts99@users.noreply.github.com>
2019-09-29 21:27:11 -0300

    Commit inicial

---
+++ src/main/java/WebAula/App.java
@@ -0,0 +1,11 @@
+package WebAula;
+
+public class App {
+    public String getGretting() {
+        return "Hello world.";
+    }
+
+    public static void main(String [] args) {
+        System.out.println(new App().getGretting());
+    }
+}
---
+++ src/test/java/WebAula/AppTest.java
@@ -0,0 +1,13 @@
+package WebAula;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testAppHasGretting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a gretting",classUnderTest.getGretting());
+    }
+}
